bfd82b1a2b6468c410a2fffec87f3fa942e85dd5
Aleksey Arteev <alarteev@yandex.ru>
2021-10-17 21:12:21 +0500

    2015 day-3 part 2

--- 2015/day-3/main.rs
+++ 2015/day-3/main.rs
@@ -29,13 +29,20 @@ impl Direction {
 }
 
 impl Position {
-    pub fn mov(&mut self, d: Direction) {
+    pub fn mov(&mut self, d: &Direction) {
         match d {
             Direction::Up => self.y = self.y - 1,
             Direction::Down => self.y = self.y + 1,
             Direction::Left => self.x = self.x - 1,
             Direction::Right => self.x = self.x + 1,
         }
+    }
+    pub fn new() -> Position {
+        Position { x: 0, y: 0 }
+    }
+
+    pub fn coordinates(&self) -> (i32, i32) {
+        (self.x, self.y)
     }
 }
 
@@ -45,18 +52,31 @@ fn main() -> io::Result<()> {
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer)?;
 
-    let mut position = Position { x: 0, y: 0 };
+    let mut position = Position::new();
 
     let mut visits: HashMap<(i32, i32), ()> = HashMap::new();
+    let mut visits_next_year: HashMap<(i32, i32), ()> = HashMap::new();
 
+    let mut positions_next = HashMap::new();
+    positions_next.insert(true, Position::new());
+    positions_next.insert(false, Position::new());
+
+    let mut who_moved = false;
     for ch in buffer {
         let dir = Direction::parse(&ch).unwrap();
-        position.mov(dir);
-        visits.insert((position.x, position.y), ());
+        // now
+        position.mov(&dir);
+        visits.insert(position.coordinates(), ());
+
+        // next year
+        let p = positions_next.get_mut(&who_moved).unwrap();
+        p.mov(&dir);
+        visits_next_year.insert(p.coordinates(), ());
+        who_moved = !who_moved;
     }
 
-    println!("position: {:?}", position);
-    println!("count: {:?}", visits.len());
+    println!("now count: {:?}", visits.len());
+    println!("next year count: {:?}", visits_next_year.len());
 
     Ok(())
 }
