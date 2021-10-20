085389bf3b40d3898634a350de0bf7d6e01eba24
Aleksey Arteev <alarteev@yandex.ru>
2021-10-20 10:42:24 +0500

    2015 day-6 part 2

--- 2015/day-6/src/main.rs
+++ 2015/day-6/src/main.rs
@@ -45,9 +45,11 @@ impl FromStr for Point {
 fn main() -> Result<(), Box<dyn Error>> {
     let re = Regex::new(r"^(toggle|turn on|turn off)\s(\d{1,3},\d{1,3})\sthrough\s(\d{1,3},\d{1,3})").unwrap();
     let mut grid = [[false; 1000]; 1000];
+    let mut grid_brightness = [[0i32; 1000]; 1000];
     let file = File::open("input.txt")?;
     let lines = io::BufReader::new(file).lines();
     let mut count = 0;
+    let mut brightness = 0;
 
     for line in lines {
         let v = line?;
@@ -61,21 +63,40 @@ fn main() -> Result<(), Box<dyn Error>> {
                 let i_idx = i as usize;
                 let j_idx = j as usize;
                 let old_state = grid[i_idx][j_idx];
+                let old_brightness = grid_brightness[i_idx][j_idx];
+
                 match &oper {
                     Operation::TOGGLE => {
                         grid[i_idx][j_idx] = !grid[i_idx][j_idx];
+                        grid_brightness[i_idx][j_idx] += 2;
                     }
-                    Operation::ON => grid[i_idx][j_idx] = true,
-                    Operation::OFF => grid[i_idx][j_idx] = false,
+
+                    Operation::ON => {
+                        grid[i_idx][j_idx] = true;
+                        grid_brightness[i_idx][j_idx] += 1;
+                    }
+
+                    Operation::OFF => {
+                        grid[i_idx][j_idx] = false;
+
+                        grid_brightness[i_idx][j_idx] -= 1;
+                        if grid_brightness[i_idx][j_idx] < 0 {
+                            grid_brightness[i_idx][j_idx] = 0;
+                        }
+                    }
                 };
                 if old_state != grid[i_idx][j_idx] {
                     if grid[i_idx][j_idx] { count += 1 } else { count -= 1 }
                 };
+                if old_brightness != grid_brightness[i_idx][j_idx] {
+                    brightness += grid_brightness[i_idx][j_idx] - old_brightness;
+                }
             }
         }
     }
 
     println!("count {}", count);
+    println!("brightness {}", brightness);
 
     Ok(())
 }
