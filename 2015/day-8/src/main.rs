44f61cef725d690cab47e23f848ced05f239a9d6
Aleksey Arteev <alarteev@yandex.ru>
2021-10-22 15:20:05 +0500

    2015 day-8 part 2

--- 2015/day-8/src/main.rs
+++ 2015/day-8/src/main.rs
@@ -1,11 +1,9 @@
 use regex::Regex;
-use std::str::FromStr;
-use std::num::ParseIntError;
 use std::io::{self, BufRead};
 use std::fs::File;
 use std::error::Error;
 
-fn calc(s: &str) -> usize {
+fn calc(s: &str) -> (usize, usize) {
     let mut sout = s.
         replace(r#"\""#, r#"""#).
         replace(r#"\\"#, r#"\"#).
@@ -22,19 +20,32 @@ fn calc(s: &str) -> usize {
             16).unwrap() as u8 as char;
         sout = sout.replace(ms, ch.to_string().as_str());
     }
-    sout.chars().count()
+
+    let mut s_encode = s.
+        replace("\\", "\\\\").
+        replace("\"", "\\\"");
+    s_encode.insert_str(0, "\"");
+    s_encode.push_str("\"");
+    (sout.chars().count(), s_encode.chars().count())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("input.txt")?;
     let lines = io::BufReader::new(file).lines();
     let mut count = 0;
+    let mut count_enc = 0;
+    let mut count_all = 0;
     for line in lines {
         let v = line?;
         let vc = v.len();
-        count += vc - calc(&v)
+        let (dec, enc) = calc(&v);
+        count += vc - dec;
+        count_all += vc;
+        count_enc += enc;
     }
+    count_enc = count_enc - count_all;
     println!("count {}", count);
+    println!("count encode {}", count_enc);
     Ok(())
 }
 
@@ -44,13 +55,13 @@ mod tests {
 
     #[test]
     fn calc_test() {
-        assert_eq!(calc("\"\""), 0);
-        assert_eq!(calc("\"abc\""), 3);
-        assert_eq!(calc(r#""aaa\"aaa""#), 7);
-        assert_eq!(calc(r#""\x27""#), 1);
-        assert_eq!(calc(r#""""#), 0);
-        assert_eq!(calc(r#""\"""#), 1);
-        assert_eq!(calc(r#""czbggabkzo\"wsnw\"voklp\"s""#), 23);
-        assert_eq!(calc(r#""vkfam\"yllr\"q\x92o\x4ebecnvhshhqe\\""#), 27);
+        assert_eq!(calc("\"\""), (0, 6));
+        assert_eq!(calc("\"abc\""), (3, 9));
+        assert_eq!(calc(r#""aaa\"aaa""#), (7, 16));
+        assert_eq!(calc(r#""\x27""#), (1, 11));
+        assert_eq!(calc(r#""""#), (0, 6));
+        assert_eq!(calc(r#""\"""#), (1, 10));
+        assert_eq!(calc(r#""czbggabkzo\"wsnw\"voklp\"s""#), (23, 38));
+        assert_eq!(calc(r#""vkfam\"yllr\"q\x92o\x4ebecnvhshhqe\\""#), (27, 50));
     }
 }
