1f8efb135db8b6ef23b8e8d748fdd39693c3d835
Aleksey Arteev <alarteev@yandex.ru>
2021-11-02 19:05:04 +0500

    2015 day-12 part 2

--- 2015/day-12/src/main.rs
+++ 2015/day-12/src/main.rs
@@ -3,24 +3,32 @@ use std::fs::File;
 use std::error::Error;
 use std::io::Read;
 
-fn scan(v: &Value) -> i64 {
+fn scan(v: &Value, ignore: bool, ignore_skip_level: bool) -> (i64, bool) {
     let mut sum = 0;
+
+    if v.is_string() && ignore && v.as_str().unwrap() == "red" && !ignore_skip_level {
+        return (0, true);
+    }
+
     if v.is_i64() || v.is_u64() {
         let v64 = v.as_i64().unwrap();
         sum += v64;
-        println!("int: {:?}", v64);
     }
 
     if v.is_f64() || v.is_f64() {
         let f64 = v.as_f64().unwrap();
         sum += f64 as i64;
-        println!("int: {:?}", f64);
     }
+
 
     if v.is_object() {
         for item in v.as_object() {
             for value in item.values() {
-                sum += scan(&value);
+                let (sum_result, was_ignored) = scan(&value, ignore, false);
+                sum += sum_result;
+                if was_ignored {
+                    return (0, false);
+                }
             }
         }
     }
@@ -28,17 +36,26 @@ fn scan(v: &Value) -> i64 {
     if v.is_array() {
         for item in v.as_array() {
             for value in item {
-                sum += scan(&value);
+                let (sum_result, was_ignored) = scan(&value, ignore, true);
+                sum += sum_result;
+                if was_ignored {
+                    return (0, false);
+                }
             }
         }
     }
-    sum
+    (sum, false)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("input.json")?;
     let v: Value = serde_json::from_reader(file)?;
-    let sum = scan(&v);
+
+    let (sum, _) = scan(&v, false, false);
     println!("sum: {}", sum);
+
+    let (sum, _) = scan(&v, true, false);
+    println!(r#"sum ignore "red": {}"#, sum);
+
     Ok(())
 }
