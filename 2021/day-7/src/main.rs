3cc85f52a1da095b807e773fbc7d44876faa130c
Aleksey Arteev <alarteev@yandex.ru>
2022-01-08 18:58:33 +0500

    2021 day-7 part-1

---
+++ 2021/day-7/src/main.rs
@@ -0,0 +1,38 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead,self};
+
+fn calculate(pos: u32, v: &Vec<u32>) -> u32 {
+    v.iter().fold(0, |s, &x| {
+        s+(pos as i32 - x as i32).abs() as u32
+    })
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("input.txt")?;
+    let lines = io::BufReader::new(file).lines().map(|x|x.unwrap());
+    
+    let mut positions:Vec<u32> = Vec::new(); 
+    for line in lines {
+        for p in line.split(",") {
+            let c: u32 = p.parse().unwrap();
+            positions.push(c);
+        }
+    }
+    let max = positions.iter().max().unwrap();
+
+    let mut m_pos : Option<u32> = None;
+    let mut fuel: Option<u32> = None;
+
+    for i in 0..=*max {
+        let c = calculate(i, &positions);
+        if fuel.is_none() || fuel.unwrap() > c {
+            m_pos = Some(i);
+            fuel=Some(c);
+        }
+    }
+    println!("fuel: {}, pos: {}", fuel.unwrap(), m_pos.unwrap());
+
+    Ok(())
+}
