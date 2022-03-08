a02d8a254d54dad9c19911a991b914d6e9fa8592
Aleksey Arteev <alarteev@yandex.ru>
2022-03-08 17:27:43 +0500

    2021 day-9 part 2

--- 2021/day-9/src/main.rs
+++ 2021/day-9/src/main.rs
@@ -1,35 +1,58 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::{BufRead,self};
+use std::io::{BufRead, self};
 
-fn is_low(x: usize, y:usize, v:&Vec<Vec<u8>>) -> bool {
+fn is_basin(x: usize, y: usize, v: &mut Vec<Vec<u8>>) -> u32 {
     let value = v[y][x];
-    if let Some(&vx) = v[y].get(x+1) {
+    if value == 9 {
+        return 0;
+    }
+    let mut result = 1;
+    v[y][x] = 9;
+
+    if let Some(_) = v[y].get(x + 1) {
+        result = result + is_basin(x + 1, y, v);
+    }
+    if let Some(_) = v[y].get(x - 1) {
+        result = result + is_basin(x - 1, y, v);
+    }
+    if let Some(_) = v.get(y + 1) {
+        result = result + is_basin(x, y + 1, v);
+    }
+    if let Some(_) = v.get(y - 1) {
+        result = result + is_basin(x, y - 1, v);
+    }
+    return result;
+}
+
+fn is_low(x: usize, y: usize, v: &Vec<Vec<u8>>) -> bool {
+    let value = v[y][x];
+    if let Some(&vx) = v[y].get(x + 1) {
         if vx <= value {
-            return false
+            return false;
         }
     }
-    if let Some(&vx) = v[y].get(x-1) {
+    if let Some(&vx) = v[y].get(x - 1) {
         if vx <= value {
-            return false
+            return false;
         }
     }
-    if let Some(vy) = v.get(y+1) {
+    if let Some(vy) = v.get(y + 1) {
         if vy[x] <= value {
-            return false
+            return false;
         }
     }
-    if let Some(vy) = v.get(y-1) {
+    if let Some(vy) = v.get(y - 1) {
         if vy[x] <= value {
-            return false
+            return false;
         }
     }
     true
 }
 
-fn main() -> Result<(),Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("input.txt")?;
-    let lines = io::BufReader::new(file).lines().map(|x|x.unwrap());
+    let lines = io::BufReader::new(file).lines().map(|x| x.unwrap());
     let mut v: Vec<Vec<u8>> = Vec::new();
     let mut x_max = 0;
     for line in lines {
@@ -37,22 +60,36 @@ fn main() -> Result<(),Box<dyn Error>> {
             c.to_string().parse::<u8>().unwrap()
         }).collect::<Vec<u8>>();
 
-        if x.len() > x_max  {
+        if x.len() > x_max {
             x_max = x.len();
         }
         v.push(x);
     }
-   
-    let mut points:u32 = 0;
+
+    let mut points: u32 = 0;
     for x in 0..x_max {
         for y in 0..v.len() {
-            let low = is_low(x,y,&v);
+            let low = is_low(x, y, &v);
             if low {
-                points += v[y][x] as u32 +1;
+                points += v[y][x] as u32 + 1;
             }
         }
     }
     println!("points: {}", points);
 
-    Ok(())    
+    let mut basins: Vec<u32> = Vec::new();
+    for x in 0..x_max {
+        for y in 0..v.len() {
+            let size = is_basin(x, y, &mut v);
+            if size > 0 {
+                basins.push(size);
+            }
+        }
+    }
+
+    basins.sort_by(|a, b| b.cmp(a));
+    let top_basins: u32 = basins.iter().take(3).product();
+    println!("size basins: {}", top_basins);
+
+    Ok(())
 }
